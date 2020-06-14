17522a827e4d7c1d57d5599f53d5e046c152f8c7
eliebeaino <eliebeaino89@gmail.com>
2020-06-14 15:24:41 +0300

    muzzle flash / added enemy
    - added mummy asset for enemy
    - added muzzle flash (placeholder)

--- My-1st-FPS/Assets/Scripts/Weapon.cs
+++ My-1st-FPS/Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float WpnDmg = 1f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
 
     void Update()
@@ -20,20 +21,28 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    // toggle raycast and check if it hits enemy to deal the damage.
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
-        {            
-            DealDmg(hit);
+        {
+            if (hit.transform.tag == "Enemy")
+            {
+                hit.transform.GetComponent<EnemyHealth>().TakeDmg(WpnDmg);
+            }
             // todo add some hit efffect for visual players
         }
         else return;  // if we don't hit anything return to avoid null reference from raycast
     }
 
-    private void DealDmg(RaycastHit hit)
-    {
-        if (hit.transform.tag == "Enemy")
-        {
-            hit.transform.GetComponent<EnemyHealth>().TakeDmg(WpnDmg);
-        }
-    }
 }
