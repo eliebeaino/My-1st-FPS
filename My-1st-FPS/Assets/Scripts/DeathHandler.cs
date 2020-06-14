a5c668a961f6099795dfafdc6db2da815e35d998
eliebeaino <eliebeaino89@gmail.com>
2020-06-14 20:42:33 +0300

    UI and scene loading
    - added few enemies for testing
    - imported text mesh pro - new UI asset
    - added game end UI
    - adding scene loading

---
+++ My-1st-FPS/Assets/Scripts/DeathHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+    [SerializeField] Canvas gameOverCanvas;
+
+    private void Start()
+    {
+        gameOverCanvas.enabled = false;
+    }
+
+    // accessed when player dead and enables the game over UI + shows the cursor - stop the game
+    public void HandleDeath()
+    {
+        gameOverCanvas.enabled = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
