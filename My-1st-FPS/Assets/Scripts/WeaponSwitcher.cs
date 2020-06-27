986f493ab8c228f888348486cb91b426603f09b8
eliebeaino <eliebeaino89@gmail.com>
2020-06-27 17:28:42 +0300

    weapon swap
    - added weapon switching with keys and mouse wheel
    - stopped weapon switching when dead

--- My-1st-FPS/Assets/Scripts/WeaponSwitcher.cs
+++ My-1st-FPS/Assets/Scripts/WeaponSwitcher.cs
@@ -1,18 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int currentWeapon = 0;
+
     void Start()
     {
-        
+        SetWeaponActive();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        int previousWeapon = currentWeapon;
+        ProcessInput();
+        ProcessMouseWheel();
+        if (previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    // Enables current weapon and disables the others
+    private void SetWeaponActive()
+    {
+        int weaponIndex = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++;
+        }
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) currentWeapon = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) currentWeapon = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) currentWeapon = 2;
+    }
+
+    private void ProcessMouseWheel()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            if (currentWeapon >= transform.childCount - 2)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount -2;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
     }
 }
